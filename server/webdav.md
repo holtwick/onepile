144a2a76b910cade6113ea4aef29599c0efc4667
Dirk Holtwick <dirk.holtwick@gmail.com>
2019-10-06 16:02:39 +0200

    WebDAV sample

---
+++ server/webdav.md
@@ -0,0 +1,8 @@
+# WebDAV Sample Installation
+
+A very widely used and open protocol for file storage is WebDAV. It is also a simple way to provide sync infrastructure on existing web servers.
+
+This is an example how to enable WebDAV on your simple web hosting that supports PHP. It is a sample installation of [Sabre/DAV](http://sabre.io/dav/install/).
+
+
+
---
+++ server/webdav/server.php
@@ -0,0 +1,30 @@
+<?php
+
+use
+    Sabre\DAV;
+
+// The autoloader
+require 'vendor/autoload.php';
+
+// Now we're creating a whole bunch of objects
+$rootDirectory = new DAV\FS\Directory('public');
+
+// The server object is responsible for making sense out of the WebDAV protocol
+$server = new DAV\Server($rootDirectory);
+
+// If your server is not on your webroot, make sure the following line has the
+// correct information
+$server->setBaseUri('/server.php');
+
+// The lock manager is reponsible for making sure users don't overwrite
+// each others changes.
+$lockBackend = new DAV\Locks\Backend\File('data/locks');
+$lockPlugin = new DAV\Locks\Plugin($lockBackend);
+$server->addPlugin($lockPlugin);
+
+// This ensures that we get a pretty index in the browser, but it is
+// optional.
+$server->addPlugin(new DAV\Browser\Plugin());
+
+// All we need to do now, is to fire up the server
+$server->exec();
