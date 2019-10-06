aeda88c16575e2159e7b3693e7326c773ae78c40
Dirk Holtwick <dirk.holtwick@gmail.com>
2019-10-06 21:08:18 +0200

    Experiment with subfolders and rewrite

--- server/onepile/server.php
+++ server/onepile/server.php
@@ -1,30 +1,27 @@
 <?php
 
-use
-    Sabre\DAV;
+  use Sabre\DAV;
 
-// The autoloader
-require 'vendor/autoload.php';
+  // The autoloader
+  require 'vendor/autoload.php';
 
-// Now we're creating a whole bunch of objects
-$rootDirectory = new DAV\FS\Directory('public');
+  // Now we're creating a whole bunch of objects
+  $rootDirectory = new DAV\FS\Directory('public');
 
-// The server object is responsible for making sense out of the WebDAV protocol
-$server = new DAV\Server($rootDirectory);
+  // The server object is responsible for making sense out of the WebDAV protocol
+  $server = new DAV\Server($rootDirectory);
 
-// If your server is not on your webroot, make sure the following line has the
-// correct information
-$server->setBaseUri('/onepile/server.php');
+  // If your server is not on your webroot, make sure the following line has the correct information
+//  $server->setBaseUri('/onepile/server.php');
+  $server->setBaseUri('/onepile/dav');
 
-// The lock manager is reponsible for making sure users don't overwrite
-// each others changes.
-$lockBackend = new DAV\Locks\Backend\File('data/locks');
-$lockPlugin = new DAV\Locks\Plugin($lockBackend);
-$server->addPlugin($lockPlugin);
+  // The lock manager is reponsible for making sure users don't overwrite each others changes.
+  $lockBackend = new DAV\Locks\Backend\File('data/locks');
+  $lockPlugin = new DAV\Locks\Plugin($lockBackend);
+  $server->addPlugin($lockPlugin);
 
-// This ensures that we get a pretty index in the browser, but it is
-// optional.
-$server->addPlugin(new DAV\Browser\Plugin());
+  // This ensures that we get a pretty index in the browser, but it is optional.
+  $server->addPlugin(new DAV\Browser\Plugin());
 
-// All we need to do now, is to fire up the server
-$server->exec();
+  // All we need to do now, is to fire up the server
+  $server->exec();
